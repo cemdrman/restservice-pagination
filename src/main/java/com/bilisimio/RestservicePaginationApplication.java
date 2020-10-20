7e30db057ff6ca25bc08238ef1d5f12f8a4c0b76
Cem Dırman <cemdrman@gmail.com>
2020-10-20 18:19:31 +0300

    init

---
+++ src/main/java/com/bilisimio/RestservicePaginationApplication.java
@@ -0,0 +1,13 @@
+package com.bilisimio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestservicePaginationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestservicePaginationApplication.class, args);
+	}
+
+}
