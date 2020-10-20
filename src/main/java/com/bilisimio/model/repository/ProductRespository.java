69d1f7646a86ad3a97f8470760c826ac9afa0413
Cem Dırman <cemdrman@gmail.com>
2020-10-20 18:32:25 +0300

    product repository

---
+++ src/main/java/com/bilisimio/model/repository/ProductRespository.java
@@ -0,0 +1,17 @@
+package com.bilisimio.model.repository;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.stereotype.Repository;
+
+import com.bilisimio.model.Product;
+
+@Repository
+public interface ProductRespository extends PagingAndSortingRepository<Product, Long> {
+
+	Page<Product> findAll(Pageable pageable);
+
+	Page<Product> findByFirstName(String firstName, Pageable pageable);
+
+}
