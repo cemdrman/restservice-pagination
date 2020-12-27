1b23e0e452a110b218f37c06dd100e80d4da9f2b
Cem Dırman <cemdrman@gmail.com>
2020-12-27 14:56:46 +0300

    stash

---
+++ src/test/java/com/bilisimio/RestservicePaginationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bilisimio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestservicePaginationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
